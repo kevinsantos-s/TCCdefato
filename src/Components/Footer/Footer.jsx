0a52fc989f7ea884ef4f0ee93f24dc131ebe1cef
Gabriel Caetano <biellcaetano11@gmail.com>
2024-09-19 11:41:37 -0300

    novas att

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,18 +1,35 @@
 import React from 'react'
-import './Footer.css'
-
+import Logo from '../../assets/KitFit.png'
+ 
+ 
 const Footer = () => {
   return (
-    <div className='footer'>
-        <p>@ 2024 KitFit. All rights reserved.</p>
-        <ul>
-            <li>Terms of services</li>
-            <li>Privacy Policy</li>
-        </ul>
-      
+   
+    <footer className="bg-black  shadow dark:bg-gray-900  text-white">
+    <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+            <a  className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                <img src={Logo} className="h-20" alt="Logo" />
+            </a>
+            <ul className="flex flex-wrap items-center mb-6 text-xl font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                <li>
+                    <a href="#inicioscroll" className="hover:underline me-4 md:me-6">Início</a>
+                </li>
+                <li>
+                    <a href="#aboutscroll" className="hover:underline me-4 md:me-6">Sobre nós</a>
+                </li>
+                <li>
+                    <a href="#contatoscroll" className="hover:underline">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a className="hover:underline">KitiFit™</a>. Todos os Direitos Reservados.</span>
     </div>
+</footer>
+   
   )
 }
-import './Footer.css'
-
+ 
+ 
 export default Footer
--- src/templates/Login/Login.jsx
+++ src/templates/Login/Login.jsx
@@ -3,30 +3,30 @@ import './Login.css'
 import Maromba from '../../assets/home2.jpg'
 import Logo from '../../assets/KitFit.png'
 import { Link } from 'react-router-dom'
-
+ 
 const Login = () => {
   return (
-       <div className='w-full h-screen flex items-start bg-[#000000] justify-center sm:'> 
+       <div className='w-full h-screen flex items-start bg-[#000000] justify-center sm:'>
          <div className='w-1/2 h-full bg-[#000000] flex flex-col p-10 justify-around items-center '>
-          <img src={Logo} className='w-1/2 h-1/4  min-w-[300px] min-h-[200px]' />
-
-          <div className='w-full flex flex-col min-h-[300px] max-h-[400px] min-w-[300px] max-w-[400px]'>
+          <img src={Logo} className='w-screen h-[200px] md:w-1/2 md:h-1/4 min-w-[300px] min-h-[200px]' />
+ 
+          <div className='w-full flex flex-col mb-[100px] min-h-[300px] max-h-[400px] min-w-[300px] max-w-[400px]'>
             <div className='w-full flex flex-col mb-5'>
               <h3 className='text-3xl font-semibold mb-2 text-[#ffffff]'>Login</h3>
               <p className='text-[#ffffff] text-base '>Bem-vindo, insira suas credenciais!</p>
               </div>
-
+ 
               <div className='w-full flex flex-col'>
                 <input className='w-full text-[#ffffff] my-2 py-2 border-b border-[#ffffff] outline-none focus:outline-none bg-[#000000]' type="email" placeholder='Email' />
-                <input className='w-full text-[#ffffff] my-2 py-2 border-b border-[#ffffff] outline-none focus:outline-none bg-[#000000]' type="password" placeholder='Senha' /> 
+                <input className='w-full text-[#ffffff] my-2 py-2 border-b border-[#ffffff] outline-none focus:outline-none bg-[#000000]' type="password" placeholder='Senha' />
               </div>
-
+ 
               <div className='w-full flex items-center justify-between'>
                 <div className='w-full flex items-center'>
                   <input type="checkbox" className='w-4 h-4 mr-2'/>
                   <p className='text-sm text-[#ffffff]'>Lembre-se de mim</p>
                 </div>
-    
+   
               </div>
               <div className='w-full flex flex-col my-4'>
               <Link to={'/home'} className='w-full bg-[#F36D04] rounded-md my-2 p-4 text-center flex items-center justify-center cursor-pointer'>
@@ -40,5 +40,5 @@ const Login = () => {
        </div>
   )
 }
-
+ 
 export default Login
