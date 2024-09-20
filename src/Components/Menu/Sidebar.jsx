a9141a3a3b05e3d915f43fc37a72d2f80e14819b
alysson-Scott <alysson120107@gmail.com>
2024-09-20 11:02:37 -0300

    sub 12

--- src/Components/Menu/Sidebar.jsx
+++ src/Components/Menu/Sidebar.jsx
@@ -4,9 +4,9 @@ import './Sidebar.css';
 const Sidebar = () => {
 
     return (
-             <div className="flex justify-around align-center px-2 py-4 border-b-2 rounded">
-              <span className="font-bold">KITFIT</span>
-              <nav className="nav flex flex-col"></nav>
+             <div className="flex flex-col justify-around align-center px-2 py-4 border-b-2 rounded">
+              <span className="font-bold ">KITFIT</span>
+            
         <div className="sidebar">
             <div className="flex justify-around items-center px-2 py-4 border-b-2 rounded">
            
@@ -14,7 +14,7 @@ const Sidebar = () => {
                 <span className="fw-bold fst-italic">KITFIT</span>
             </div>
 
-            <nav className="nav flex-column">
+            <nav className="align-center justify-center flex flex-col">
                 <Link className="nav-link" aria-current="page" to={'/home'}>Dashboard</Link>
                 <Link className="nav-link" to={'/usuarioslista'}>Usuário</Link>
                 <Link className="nav-link" to={'/produto'}>Kit's</Link>
